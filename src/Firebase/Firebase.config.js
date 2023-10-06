0dbf2bec61e4182275b1d36d22daaf7fdf6d1f83
sabbir20201 <sabbirsarkar674@gmail.com>
2023-10-06 18:19:24 +0600

    updated firebase confg

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,13 +5,15 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCLMpwo-bK83enIc5kamRiWQRRHsXQszak",
-  authDomain: "auth-projects-c7066.firebaseapp.com",
-  projectId: "auth-projects-c7066",
-  storageBucket: "auth-projects-c7066.appspot.com",
-  messagingSenderId: "788620642027",
-  appId: "1:788620642027:web:6273fd8469efe31cd760ae"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
